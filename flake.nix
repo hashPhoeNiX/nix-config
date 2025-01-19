86b1e630b0f681940607080b8753ce3e53c79fc4
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-01-19 16:46:56 +0100

    playing around with nixos, home-manager and flakes

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+	url = "github:nixos/nixpkgs/release-24.05";
+    };
+    home-manager = {
+        url = "github:nix-community/home-manager/release-24.05";
+ 	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    #hyprland = {
+    #    url = "github:hyprwm/Hyprland";
+    #	inputs.nixpkgs.follows = "nixpkgs";
+    #};
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+     let
+        system = "aarch64-linux";
+        pkgs = import nixpkgs {
+           inherit system;
+           config.allowUnfree = true;
+        };
+        lib = nixpkgs.lib;
+     in {
+        nixosConfigurations = {
+	   pelumi = lib.nixosSystem {
+	       inherit system;
+	       modules = [ 
+		  ./configuration.nix
+	       ];
+	   };
+           test = lib.nixosSystem {
+	       inherit system;
+	       modules = [ 
+		  ./configuration.nix
+		  #home-manager.nixosModules.home-manager 
+		  #{
+                  #   home-manager = {
+		  #      useGlobalPkgs = true;
+                  #      useUserPackages = true;
+		  #      users.test = import ./home.nix;
+		  #      #users.test = {
+		  #      #   imports = [ (import ./home.nix) ];
+		  #      #   home.stateVersion = "24.05";
+		  #      #};
+		  #      # Optionally, use home-manager.extraSpecialArgs to pass
+                  #      # arguments to home.nix
+		  #   };
+		  #}
+	       ];
+	   };
+	};
+    };
+}
