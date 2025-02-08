a7abd58715ad5e5a52a924a8959105caff56b6af
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-02-08 20:05:38 +0100

    add nixcats home manager module

---
+++ home/nixcats-home.nix
@@ -0,0 +1,205 @@
+{ config, lib, inputs, ... }: let
+  utils = inputs.nixCats.utils;
+in {
+  imports = [
+    inputs.nixCats.homeModule
+  ];
+  config = {
+    # this value, nixCats is the defaultPackageName you pass to mkNixosModules
+    # it will be the namespace for your options.
+    nixCats = {
+      enable = true;
+      nixpkgs_version = inputs.nixpkgs;
+      # this will add the overlays from ./overlays and also,
+      # add any plugins in inputs named "plugins-pluginName" to pkgs.neovimPlugins
+      # It will not apply to overall system, just nixCats.
+      addOverlays = /* (import ./overlays inputs) ++ */ [
+        (utils.standardPluginOverlay inputs)
+      ];
+      # see the packageDefinitions below.
+      # This says which of those to install.
+      packageNames = [ "nvim" "testnvim" ];
+
+      luaPath = "${./.}";
+
+      # the .replace vs .merge options are for modules based on existing configurations,
+      # they refer to how multiple categoryDefinitions get merged together by the module.
+      # for useage of this section, refer to :h nixCats.flake.outputs.categories
+      categoryDefinitions.replace = ({ pkgs, settings, categories, extra, name, mkNvimPlugin, ... }@packageDef: {
+        lspsAndRuntimeDeps = with pkgs; {
+      	  general = [
+      	    universal-ctags
+      	    curl
+      	    lazygit
+      	    ripgrep
+      	    fd
+      	    stdenv.cc.cc
+      	    lua-language-server
+      	    nil # I would go for nixd but lazy chooses this one idk
+      	    stylua
+      	  ];
+      	};
+
+        startupPlugins = with pkgs.vimPlugins; {
+            general = [
+              # LazyVim
+              lazy-nvim
+              LazyVim
+              bufferline-nvim
+              lazydev-nvim
+              cmp-buffer
+              cmp-nvim-lsp
+              cmp-path
+              cmp_luasnip
+              conform-nvim
+              dashboard-nvim
+              dressing-nvim
+              flash-nvim
+              friendly-snippets
+              gitsigns-nvim
+              indent-blankline-nvim
+              lualine-nvim
+              neo-tree-nvim
+              neoconf-nvim
+              neodev-nvim
+              noice-nvim
+              nui-nvim
+              nvim-cmp
+              nvim-lint
+              nvim-lspconfig
+              nvim-notify
+              nvim-spectre
+              nvim-treesitter-context
+              nvim-treesitter-textobjects
+              nvim-ts-autotag
+              nvim-ts-context-commentstring
+              nvim-web-devicons
+              persistence-nvim
+              plenary-nvim
+              telescope-fzf-native-nvim
+              telescope-nvim
+              todo-comments-nvim
+              tokyonight-nvim
+              trouble-nvim
+              vim-illuminate
+              vim-startuptime
+              which-key-nvim
+              snacks-nvim
+              nvim-treesitter-textobjects
+              nvim-treesitter.withAllGrammars
+              # This is for if you only want some of the grammars
+              # (nvim-treesitter.withPlugins (
+              #   plugins: with plugins; [
+              #     nix
+              #     lua
+              #   ]
+              # ))
+
+              # sometimes you have to fix some names
+              { plugin = luasnip; name = "LuaSnip"; }
+              { plugin = catppuccin-nvim; name = "catppuccin"; }
+              { plugin = mini-ai; name = "mini.ai"; }
+              { plugin = mini-icons; name = "mini.icons"; }
+              { plugin = mini-bufremove; name = "mini.bufremove"; }
+              { plugin = mini-comment; name = "mini.comment"; }
+              { plugin = mini-indentscope; name = "mini.indentscope"; }
+              { plugin = mini-pairs; name = "mini.pairs"; }
+              { plugin = mini-surround; name = "mini.surround"; }
+              # you could do this within the lazy spec instead if you wanted
+              # and get the new names from `:NixCats pawsible` debug command
+            ];
+
+          # themer = with pkgs; [
+          #   # you can even make subcategories based on categories and settings sets!
+          #   (builtins.getAttr packageDef.categories.colorscheme {
+          #       "onedark" = onedark-vim;
+          #       "catppuccin" = catppuccin-nvim;
+          #       "catppuccin-mocha" = catppuccin-nvim;
+          #       "tokyonight" = tokyonight-nvim;
+          #       "tokyonight-day" = tokyonight-nvim;
+          #     }
+          #   )
+          # ];
+        };
+        optionalPlugins = {
+          general = [];
+        };
+        # shared libraries to be added to LD_LIBRARY_PATH
+        # variable available to nvim runtime
+        sharedLibraries = {
+          general = with pkgs; [
+            # libgit2
+          ];
+        };
+        environmentVariables = {
+          test = {
+            CATTESTVAR = "It worked!";
+          };
+        };
+        extraWrapperArgs = {
+          test = [
+            '' --set CATTESTVAR2 "It worked again!"''
+          ];
+        };
+        # lists of the functions you would have passed to
+        # python.withPackages or lua.withPackages
+
+        # get the path to this python environment
+        # in your lua config via
+        # vim.g.python3_host_prog
+        # or run from nvim terminal via :!<packagename>-python3
+        extraPython3Packages = {
+          test = (_:[]);
+        };
+        # populates $LUA_PATH and $LUA_CPATH
+        extraLuaPackages = {
+          test = [ (_:[]) ];
+        };
+      });
+
+      # see :help nixCats.flake.outputs.packageDefinitions
+      packageDefinitions.replace = {
+        # These are the names of your packages
+        # you can include as many as you wish.
+        nvim = {pkgs , ... }: {
+          # they contain a settings set defined above
+          # see :help nixCats.flake.outputs.settings
+          settings = {
+            wrapRc = true;
+            # IMPORTANT:
+            # your alias may not conflict with your other packages.
+            aliases = [ "vim" "homeVim" ];
+            # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+          };
+          # and a set of categories that you want
+          # (and other information to pass to lua)
+          categories = {
+            general = true;
+            test = true;
+            example = {
+              youCan = "add more than just booleans";
+              toThisSet = [
+                "and the contents of this categories set"
+                "will be accessible to your lua with"
+                "nixCats('path.to.value')"
+                "see :help nixCats"
+              ];
+            };
+          };
+        };
+	testnvim = { pkgs, mkNvimPlugin, ... }: {
+      	  settings = {
+      	    wrapRc = false;
+      	    unwrappedCfgPath = "/absolute/path/to/config";
+      	  };
+      	  categories = {
+      	    general = true;
+      	    test = false;
+      	  };
+      	  extra = {};
+      	};
+      };
+    };
+  };
+
+}
