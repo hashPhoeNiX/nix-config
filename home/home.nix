081a147eaaa44337d78dcef88555fa33bf2c8e66
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-02-20 20:47:25 +0100

    update nixCats

--- home/home.nix
+++ home/home.nix
@@ -20,7 +20,7 @@
   #fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     htop
-    neovim
+    #neovim
     brave
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,6 +109,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    neovim
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
