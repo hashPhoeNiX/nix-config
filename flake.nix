3ec4083ee7960652549a914a3b7ad50386ff8339
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-02-20 18:48:39 +0100

    add nixCats

--- flake.nix
+++ flake.nix
@@ -30,13 +30,13 @@
      in {
         nixosConfigurations = {
 	   pelumi = lib.nixosSystem {
-	       inherit system;
+	       inherit system inputs;
 	       modules = [ 
 		  ./nixos/configuration.nix
 	       ];
 	   };
            test = lib.nixosSystem {
-	       inherit system;
+	       inherit system inputs;
 	       modules = [ 
 		  ./nixos/configuration.nix
 		  home-manager.nixosModules.home-manager 
