c3d63401dc59d4276fe9cab4ee1f0077733ac671
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-02-21 15:56:38 +0100

    update home manager nixCats

--- nixcats/default.nix
+++ nixcats/default.nix
@@ -42,7 +42,7 @@ in {
         startupPlugins = with pkgs.vimPlugins; {
           general = [
             LazyVim
-            lazy-nvim
+            #lazy-nvim
             LazyVim
             bufferline-nvim
             lazydev-nvim
@@ -83,7 +83,7 @@ in {
             vim-illuminate
             vim-startuptime
             which-key-nvim
-            snacks-nvim
+            #snacks-nvim
             nvim-treesitter-textobjects
             nvim-treesitter.withAllGrammars
             # This is for if you only want some of the grammars
@@ -97,13 +97,13 @@ in {
             # sometimes you have to fix some names
             { plugin = luasnip; name = "LuaSnip"; }
             { plugin = catppuccin-nvim; name = "catppuccin"; }
-            { plugin = mini-ai; name = "mini.ai"; }
-            { plugin = mini-icons; name = "mini.icons"; }
-            { plugin = mini-bufremove; name = "mini.bufremove"; }
-            { plugin = mini-comment; name = "mini.comment"; }
-            { plugin = mini-indentscope; name = "mini.indentscope"; }
-            { plugin = mini-pairs; name = "mini.pairs"; }
-            { plugin = mini-surround; name = "mini.surround"; }
+            #{ plugin = mini-ai; name = "mini.ai"; }
+            #{ plugin = mini-icons; name = "mini.icons"; }
+            #{ plugin = mini-bufremove; name = "mini.bufremove"; }
+            #{ plugin = mini-comment; name = "mini.comment"; }
+            #{ plugin = mini-indentscope; name = "mini.indentscope"; }
+            #{ plugin = mini-pairs; name = "mini.pairs"; }
+            #{ plugin = mini-surround; name = "mini.surround"; }
             # you could do this within the lazy spec instead if you wanted
             # and get the new names from `:NixCats pawsible` debug command
           ];
