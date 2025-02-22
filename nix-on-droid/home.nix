8c1f58ee7fe621f914fe513e71fccaacb9afacd3
Oluwapelumi Adeosun <adeosun.pelumi5@gmail.com>
2025-02-22 02:39:46 +0100

    add nix-on-droid configuration to flake

---
+++ nix-on-droid/home.nix
@@ -0,0 +1,28 @@
+{ config, lib, pkgs, ... }:
+{
+  # Read the changelog before changing this value
+  home.stateVersion = "23.11";
+  imports = [
+    ../nixcats
+  ];
+  # insert home-manager config
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion = {
+      enable = true;
+      #strategy = [ "completion" ]; #Error
+    };
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "nix-on-droid switch --flake ~/nix-config/nix-on-droid/flake.nix";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "sorin"; #"robbyrussell";
+    };
+  };
+};
+
